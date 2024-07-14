730cb227b6b2d0c1af6062b84c7aba484967b5ac
JohnAleo <khudaynazrovs@gmail.com>
2024-07-14 19:12:59 +0900

    sss

---
+++ server.js
@@ -0,0 +1,35 @@
+const express= require("express")
+const app= express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:false}));
+
+app.use("/add-new-motor", (req, res,next) =>{
+ res.send(
+    `
+    <h1>New Motor</h1>
+    <form action="/motor" method="POST">
+        <input type="text"  placeholder="motor name"/> <br>
+         <input type="text"  placeholder="price"/> <br>
+        <input type="text" placeholder="ID"/> <br>
+        <input type="submit" value="send"/>
+    </form>
+    `
+ )   
+})
+
+app.use("/student", (req,res,next)=>{
+console.log(req.body);
+//res.send('<h1>Student List</h1>')
+res.redirect("/")
+})
+app.use("/",(req,res,next) => {
+    console.log("MidleWare...");
+    res.send('<h1>Motor</h1>'
+    );
+});
+
+const PORT = process.env.PORT || 5006;
+app.listen(PORT, ()=>{
+    console.log("Server running on port:", PORT);
+});
